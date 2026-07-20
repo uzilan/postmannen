459605dc45612fcf30c5c2f0ee52f33bdeb58674
Uzi Landsmann <uzi.landsmann.external@worldline.com>
2026-07-20 14:17:40 +0200

    docs: annotate environment routes for OpenAPI

--- src/main/kotlin/postmannen/server/EnvironmentRoutes.kt
+++ src/main/kotlin/postmannen/server/EnvironmentRoutes.kt
@@ -1,19 +1,24 @@
 package postmannen.server
 
 import io.ktor.http.HttpStatusCode
+import io.ktor.openapi.jsonSchema
 import io.ktor.server.request.receive
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
+import io.ktor.server.routing.openapi.describe
 import io.ktor.server.routing.post
 import io.ktor.server.routing.put
+import io.ktor.utils.io.ExperimentalKtorApi
 import kotlinx.serialization.Serializable
+import postmannen.model.Environment
 import postmannen.model.EnvironmentDetail
 import postmannen.service.PostmanApiService
 
 @Serializable
 data class CreateEnvironmentRequest(val workspaceId: String, val name: String)
 
+@OptIn(ExperimentalKtorApi::class)
 fun Route.environmentRoutes(service: PostmanApiService) {
     get("/api/environments") {
         val workspaceId = call.request.queryParameters["workspaceId"]
@@ -22,21 +27,93 @@ fun Route.environmentRoutes(service: PostmanApiService) {
             return@get
         }
         call.respondResult(service.getEnvironments(workspaceId))
+    }.describe {
+        summary = "List environments for a workspace"
+        parameters {
+            query("workspaceId") {
+                description = "The workspace id to list environments for"
+                required = true
+            }
+        }
+        responses {
+            HttpStatusCode.OK {
+                description = "The workspace's environments"
+                schema = jsonSchema<List<Environment>>()
+            }
+            HttpStatusCode.BadRequest {
+                description = "workspaceId query parameter is missing"
+            }
+            HttpStatusCode.BadGateway {
+                description = "The Postman API request failed"
+            }
+        }
     }
 
     get("/api/environments/{uid}") {
         val uid = call.parameters["uid"]!!
         call.respondResult(service.getEnvironmentDetail(uid))
+    }.describe {
+        summary = "Get an environment's key/value details"
+        parameters {
+            path("uid") {
+                description = "The environment uid"
+                required = true
+            }
+        }
+        responses {
+            HttpStatusCode.OK {
+                description = "The environment detail"
+                schema = jsonSchema<EnvironmentDetail>()
+            }
+            HttpStatusCode.BadGateway {
+                description = "The Postman API request failed"
+            }
+        }
     }
 
     put("/api/environments/{uid}") {
         val uid = call.parameters["uid"]!!
         val detail = call.receive<EnvironmentDetail>().copy(uid = uid)
         call.respondUnitResult(service.updateEnvironment(detail))
+    }.describe {
+        summary = "Update an environment's key/value details"
+        parameters {
+            path("uid") {
+                description = "The environment uid"
+                required = true
+            }
+        }
+        requestBody {
+            description = "The environment's new key/value details"
+            schema = jsonSchema<EnvironmentDetail>()
+        }
+        responses {
+            HttpStatusCode.NoContent {
+                description = "The environment was updated"
+            }
+            HttpStatusCode.BadGateway {
+                description = "The Postman API request failed"
+            }
+        }
     }
 
     post("/api/environments") {
         val request = call.receive<CreateEnvironmentRequest>()
         call.respondResult(service.createEnvironment(request.workspaceId, request.name), HttpStatusCode.Created)
+    }.describe {
+        summary = "Create an environment"
+        requestBody {
+            description = "The workspace to create the environment in and its name"
+            schema = jsonSchema<CreateEnvironmentRequest>()
+        }
+        responses {
+            HttpStatusCode.Created {
+                description = "The created environment"
+                schema = jsonSchema<EnvironmentDetail>()
+            }
+            HttpStatusCode.BadGateway {
+                description = "The Postman API request failed"
+            }
+        }
     }
 }
