96c1ceb6b61ae0cf47cb6672893b40a5257a17b9
Uzi Landsmann <uzi.landsmann.external@worldline.com>
2026-07-20 14:10:30 +0200

    chore: bump Ktor to 3.5.1 and add ktor-server-routing-openapi

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,20 +20,21 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
 
-    implementation("io.ktor:ktor-client-cio:3.1.0")
-    implementation("io.ktor:ktor-client-content-negotiation:3.1.0")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:3.1.0")
+    implementation("io.ktor:ktor-client-cio:3.5.1")
+    implementation("io.ktor:ktor-client-content-negotiation:3.5.1")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:3.5.1")
 
-    implementation("io.ktor:ktor-server-core:3.1.0")
-    implementation("io.ktor:ktor-server-cio:3.1.0")
-    implementation("io.ktor:ktor-server-content-negotiation:3.1.0")
+    implementation("io.ktor:ktor-server-core:3.5.1")
+    implementation("io.ktor:ktor-server-cio:3.5.1")
+    implementation("io.ktor:ktor-server-content-negotiation:3.5.1")
+    implementation("io.ktor:ktor-server-routing-openapi:3.5.1")
     implementation("ch.qos.logback:logback-classic:1.5.16")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test:2.4.0")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.1")
-    testImplementation("io.ktor:ktor-server-test-host:3.1.0")
-    testImplementation("io.ktor:ktor-client-content-negotiation:3.1.0")
-    testImplementation("io.ktor:ktor-client-mock:3.1.0")
+    testImplementation("io.ktor:ktor-server-test-host:3.5.1")
+    testImplementation("io.ktor:ktor-client-content-negotiation:3.5.1")
+    testImplementation("io.ktor:ktor-client-mock:3.5.1")
 }
 
 application {
